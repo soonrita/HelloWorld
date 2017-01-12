7469a780f8c94e07ff826a7533917c8db7d21b3d
Soonrita Immalaraju <soonrita@Soonritas-MacBook-Pro.local>
2017-01-12 07:07:51 -0500

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by soonrita on 1/11/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
